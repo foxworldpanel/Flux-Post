4999ddb4c1d276ec2e68757b0a3024278913a9c1
foxworldpanel <foxworldpanel@gmail.com>
2026-08-16 06:53:33 -0300

    Update index.js

--- workers/render-worker/index.js
+++ workers/render-worker/index.js
@@ -28,12 +28,10 @@ const BRIDGE_URL = `${supabaseUrl}/functions/v1/render-bridge`;
 const client = axios.create({
   baseURL: BRIDGE_URL,
   headers: {
-    headers: {
-  'x-render-worker-secret': workerSecret,
-  'Content-Type': 'application/json',
-  'apikey': supabaseAnonKey,
-  'Authorization': `Bearer ${supabaseAnonKey}`
-}
+    'x-render-worker-secret': workerSecret,
+    'Content-Type': 'application/json',
+    'apikey': supabaseAnonKey,
+    'Authorization': `Bearer ${supabaseAnonKey}`
   }
 });
 
